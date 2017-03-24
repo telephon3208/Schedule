a9b646b56c588b631493befdc7c0c26e7167661b
Maria Yurchenko <krossovki@li.ru>
2017-03-24 19:46:18 +0300

    Отработала в отдельном модуле как работать с фрагментами

---
+++ testfragments/src/main/java/com/masha/testfragments/MainActivity.java
@@ -0,0 +1,80 @@
+package com.masha.testfragments;
+
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        fragmentsInit();
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    private void fragmentsInit() {
+        FragmentManager myFragmentManager = getSupportFragmentManager();
+
+        Fragment1 fragment1 = new Fragment1();
+        Fragment2 fragment2 = new Fragment2();
+
+      //  Bundle bundle = new Bundle();
+       // fragment1.setArguments(bundle);
+
+        FragmentTransaction fragmentTransaction = myFragmentManager
+                .beginTransaction();
+        fragmentTransaction.add(R.id.fragment1, fragment1);
+        Configuration configuration = getResources().getConfiguration();
+
+        if (configuration.smallestScreenWidthDp >= 600) {
+            fragmentTransaction.add(R.id.fragment2, fragment2);
+        }
+
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
